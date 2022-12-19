1602a3a4e92a780c053fde38ba8e9236052e2db3
Brett Bates <brettbates86@gmail.com>
2022-12-19 13:12:10 -0600

    add functions to show roles and employees

--- server.js
+++ server.js
@@ -110,5 +110,55 @@ const promptUser = () => {
             connection.end()
         };
     });
+};
 
-}
+showDepartments = () => {
+    console.log('Showing all departments...\n');
+    const sql = `SELECT department.id AS id, department.name AS department FROM department`;
+
+    connection.promise().query(sql, (err, rows) => {
+        if (err) throw err;
+        console.table(rows):
+        promptUser();
+    });
+};
+
+// FUNCTION SHOWS ALL ROLES //
+
+showRoles = () => {
+    console.log('Showing all roles...\n');
+
+    const sql = `SELECT role.id, role.title, department.name AS department
+                 FROM role
+                 INNER JOIN department ON role.depatment_id = department.id`;
+
+    connection.promise().query(sql, (err, rows) => {
+        if (err) throw err;
+        console.table(rows);
+        promptUser();
+    })
+};
+
+// FUNCTION TO SHOW ALL EMPLOYESS //
+
+showEmployees = () => {
+    console.log('Showing all employees...\n');
+    const sql = `SELECT employee.id, 
+    employee.first_name, 
+    employee.last_name, 
+    role.title, 
+    department.name AS department,
+    role.salary, 
+    CONCAT (manager.first_name, " ", manager.last_name) AS manager
+FROM employee
+    LEFT JOIN role ON employee.role_id = role.id
+    LEFT JOIN department ON role.department_id = department.id
+    LEFT JOIN employee manager ON employee.manager_id = manager.id`;
+
+connection.promise().query(sql, (err, rows) => {
+if (err) throw err; 
+console.table(rows);
+promptUser();
+});
+};
+
